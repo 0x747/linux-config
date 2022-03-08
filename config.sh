5ed92ddf92e4ede8b87383ba10906bc172340dd9
777 <82454512+generalgope@users.noreply.github.com>
2022-03-08 22:10:43 +0530

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,3 +1,27 @@
+sudo apt update
+sudo apt upgrade -y
+
+# Eye candy
 sudo apt install neofetch -y
+sudo apt install cmatrix -y
+sudo apt install hollywood -y
+
+# Programming Languages
 sudo apt install default-jdk -y
 sudo apt install default-jre -y
+sudo apt install python3
+
+# Bashtop
+sudo add-apt-repository ppa:bashtop-monitor/bashtop
+sudo apt update
+sudo apt install bashtop
+
+# Tor 
+sudo add-apt-repository ppa:micahflee/ppa
+sudo apt update
+sudo apt install torbrowser-launcher -y
+
+# qbittorent
+sudo add-apt-repository ppa:qbittorrent-team/qbittorrent-stable
+sudo apt update
+sudo apt install qbittorent -y
